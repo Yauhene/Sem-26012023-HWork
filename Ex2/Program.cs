846d84661599b528fda6c703ee6dddd5cadbc5d6
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-01-29 17:16:40 +0300

    add Gone

---
+++ Ex2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("============================");
+Console.Write("Введите первое число :");
+if(int.TryParse(Console.ReadLine(), out int a1))
+
+Console.Write("Введите второе число :");
+if(int.TryParse(Console.ReadLine(), out int a2))
+
+    if(a1 != a2)
+    {
+        if((a1 - a2) > 0) Console.WriteLine("Число " + a1 + " больше чем " + a2);
+        else           Console.WriteLine("Число " + a2 + " больше чем " + a1);
+    }
+    else 
+    {
+        Console.WriteLine("Вы ввели одно и то же число дважды");
+    }
+Console.WriteLine("============================");
